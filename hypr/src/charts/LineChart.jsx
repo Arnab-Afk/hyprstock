724d136d251bfb52fc4a625ddab94b43b2b1baa2
Unchanted <omkkdwivedi@gmail.com>
2024-10-26 12:34:08 +0530

    Stats

---
+++ hypr/src/charts/LineChart.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Line } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const LineChart = () => {
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        backgroundColor: "rgb(75, 192, 192)",
+        borderColor: "rgba(75, 192, 192, 0.7)",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto p-4">
+      <Line data={data} options={options} />
+    </div>
+  );
+};
+
+export default LineChart;
--- hypr/src/screens/Stats.jsx
+++ hypr/src/screens/Stats.jsx
@@ -1,7 +1,14 @@
 // import React from "react";
+import LineChart from "../charts/LineChart";
+import PiChart from "../charts/PiChart";
 
 const StatsScreen = () => {
-  return <div className="h-screen bg-slate-700">Hello stats</div>;
+  return (
+    <div className="h-screen ">
+      <PiChart />
+      <LineChart />
+    </div>
+  );
 };
 
 export default StatsScreen;
